7ed045bbb041da9827a11a3ff4614f4c6b49cfc9
Varun KH <varun.kh16@gmail.com>
2024-02-24 23:25:31 +0530

    Added mongo pod and mongoose model for user

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,29 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage("Email must be valid!"),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters!")
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
-    console.info("Creating a user ....");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new BadRequestError("Email in use");
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+    return res.status(201).send(user);
 });
 
 export { router as signupRouter };
