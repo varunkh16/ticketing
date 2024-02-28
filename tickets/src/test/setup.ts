6238208c65a3ac1cfc1758eac9859cea90d8350c
Varun KH <varun.kh16@gmail.com>
2024-02-28 22:34:56 +0530

    Initialized tickets micro service

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,52 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose, { ConnectOptions } from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+    var signup: () => Promise<string[]>;
+}
+
+let mongo: any;
+beforeAll(async () => {
+    process.env.JWT_KEY = 'asdfghjkl';
+
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+        UseNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true,
+    } as ConnectOptions);
+});
+
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+    await mongoose.connection.close();
+});
+
+global.signup = async () => {
+    const email = "test@test.com";
+    const password = "password";
+    const response = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email, password
+        })
+        .expect(201);
+    
+    const cookie = response.get("Set-Cookie");
+
+    return cookie;
+}
